b931744a47831e2914ec919432a7a936b4318df7
Douglas Confere <dougconfere@gmail.com>
2015-03-02 22:22:04 -0500

    Initial
    Simple moving sprite test, no controller input or rotation yet.  Began
    making classes for game entities

---
+++ SpriteTest/Game1.cs
@@ -0,0 +1,147 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace SpriteTest
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        KeyboardState   OldKeyboardState,           // We will compare the new and old keyboard states so we know if a key was freshly pressed, held, or released
+                        CurrentKeyboardState;
+        List<Entity> Entities;                      // We shouldn't need random access to our game entities, so a list will suffice and be more efficient than an array.
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+
+        // This is a texture we can render.
+        Texture2D myTexture;
+
+        // Set the coordinates to draw the sprite at.
+        Vector2 spritePosition = Vector2.Zero;
+
+        // Store some information about the sprite's motion.
+        Vector2 spriteSpeed = new Vector2(50.0f, 50.0f);
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            myTexture = Content.Load<Texture2D>("Redbig");
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            // TODO: Add your update logic here
+            UpdateSprite(gameTime);
+            base.Update(gameTime);
+        }
+
+        void UpdateSprite(GameTime gameTime)
+        {
+            // Move the sprite by speed, scaled by elapsed time.
+            spritePosition +=
+                spriteSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            int MaxX =
+                graphics.GraphicsDevice.Viewport.Width - myTexture.Width;
+            int MinX = 0;
+            int MaxY =
+                graphics.GraphicsDevice.Viewport.Height - myTexture.Height;
+            int MinY = 0;
+
+            // Check for bounce.
+            if (spritePosition.X > MaxX)
+            {
+                spriteSpeed.X *= -1;
+                spritePosition.X = MaxX;
+            }
+
+            else if (spritePosition.X < MinX)
+            {
+                spriteSpeed.X *= -1;
+                spritePosition.X = MinX;
+            }
+
+            if (spritePosition.Y > MaxY)
+            {
+                spriteSpeed.Y *= -1;
+                spritePosition.Y = MaxY;
+            }
+
+            else if (spritePosition.Y < MinY)
+            {
+                spriteSpeed.Y *= -1;
+                spritePosition.Y = MinY;
+            }
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            // Draw the sprite.
+            spriteBatch.Begin(SpriteSortMode.BackToFront, BlendState.AlphaBlend);
+            spriteBatch.Draw(myTexture, spritePosition, Color.White);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ SpriteTest/GameEntities.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace SpriteTest
+{
+    class Entity
+    {
+        Vector2 Position;
+        Vector2 Velocity;
+        Vector2 Acceleration;
+        float Rotation;
+    }
+
+    class Mobile : Entity
+    {
+        float ThrustAcceleration;       // Not only the "gas" if the entity wants to move, but also the "brake" in the event of stopping or inertial dampening.
+        float Hull;                     // i.e. HP, health, life, etc.
+        bool DampenInnertia;            // If true, the object will automatically brake itself if not accelerating.
+    }
+
+    class Pickup : Mobile
+    {
+        float PickupRadius;             // If within this distance to pickup, you pick it up
+    }
+
+    class Projectile : Mobile
+    {
+        float BaseDamage;               // Base damage the projectile does to a target on collision
+        float HullDamageCoef;           // Coefficient if it hits only hull
+        float ShieldDamageCoef;         // Coefficient if it hits shields
+        float ExplosionRadius;          // If > 0, the projectile explodes on contact and damages entities within this radius.
+    }
+
+    class Vessel : Mobile
+    {        
+        float RotationSpeed;            // In rads/s
+        float Armor;                    // Will dampen damage to the hull
+    }
+
+    class Shielded : Vessel
+    {
+        float MaxShields;               // Maximum shield amount, shields will recharge to this amount
+        float Shields;                  // Current amount of shields.  Below 20%, damage bleeds through to hull
+        float ShieldRechargeRate;       // Rate in shields/s of recharge
+    }
+}
